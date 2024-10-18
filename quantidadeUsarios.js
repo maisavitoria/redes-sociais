b27c26cf71b81fae8bd74f8a80be47685eb860ae
maisavitoria <maisa.carvalho.souza@escola.pr.gov.br>
2024-10-18 10:16:07 -0300

    Update quantidadeUsarios.js

--- quantidadeUsarios.js
+++ quantidadeUsarios.js
@@ -1,4 +1,5 @@
 async function quantidadeUsuarios() {
   const url = 'https://raw.githubusercontent.com/guilhermeomrails/api/main/numero-usuarios.json'
   const res = await fetch(url)
+  const dados = await res.json()
 }
