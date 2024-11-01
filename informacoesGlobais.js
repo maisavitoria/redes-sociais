279197f1a8c6f82d462943be99f98d02f783b781
maisavitoria <maisa.carvalho.souza@escola.pr.gov.br>
2024-11-01 10:11:40 -0300

    Update informacoesGlobais.js

--- informacoesGlobais.js
+++ informacoesGlobais.js
@@ -1,16 +1,20 @@
-const url = `https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json`
-Console.log(url);
-async function visualizarinformacoesGlobais () {
-    const res = await fetch (url) ;
+const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json'
+
+async function vizualizarInformacoesGlobais() {
+    const res = await fetch(url)
     const dados = await res.json()
-    const paragrafo = document.createelement (`p`) 
-    paragrafo.classlist.add(`graficos-container___texto`)
-paragrafo.innerHTML = `voce sabia que o mundo Tem 
-<span>${dados.total_pessoas_mundo}</span> de pessoas e 
-    que aproxidamente <span>${dados.total_pessoas_conectados} 
-    estão conectadas}</span>estão conectadas em alguma rede social e passam em média
-    <span>${dados.tempo_medio}</span> horas conectadas.`
-const container = document.getelementById (`graficos-container`)
-container .appendChld(paragrafo)
+    const pessoasConectadas = (dados.total_pessoas_conectadas / 1e9)
+    const pessoasNoMundo = (dados.total_pessoas_mundo / 1e9)
+    const horas = parseInt(dados.tempo_medio)
+    const minutos = Math.round((dados.tempo_medio - horas) * 100)
+    const porcentagemConectada = ((pessoasConectadas / pessoasNoMundo ) * 100).toFixed(2)
+
+    const paragrafo = document.createElement('p')
+    paragrafo.classList.add('graficos-container__texto')
+    paragrafo.innerHTML = `Você sabia que o mundo tem <span>${pessoasNoMundo} bilhões</span> de pessoas e que aproximadamente <span>${pessoasConectadas} bilhões</span> estão conectadas em alguma rede social e passam em média <span>${horas} horas</span> e <span>${minutos} minutos</span> conectadas.<br>Isso significa que aproximadamente <span>${porcentagemConectada}%</span> de pessoas estão conectadas em alguma rede social.`
+
+    const container = document.getElementById('graficos-container')
+    container.appendChild(paragrafo)
 }
-visualizarinformacoesGlobais()
+
+vizualizarInformacoesGlobais()
